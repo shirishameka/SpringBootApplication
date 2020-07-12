0073f6ec23cf9019c4d8639cbf52e0776942869a
Shirisha <shirisha@apples-MacBook-Pro.local>
2020-07-12 19:59:23 +0530

    POST,GET APIs

--- src/main/java/com/example/MyApp/demo/Controller.java
+++ src/main/java/com/example/MyApp/demo/Controller.java
@@ -1,12 +1,15 @@
 package com.example.MyApp.demo;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 public class Controller {
+
+    Db db=new Db();
 
     @GetMapping("/hi")
     public String sayHello() {
@@ -24,4 +27,31 @@ public class Controller {
         User user= new User(1,"Siri",25);
         return user;
     }
+
+    //Create New user -POST
+    @PostMapping("/users")
+    public boolean createUser(@RequestBody User user) {
+    return db.addUser(user);
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<User> getALlUsers(){
+        return db.getAllUsers();
+    }
+
+    //Search a user by user_id
+    //localhost:8090/search/user?id=101 -query param
+    @GetMapping("/search/user")
+    public User findUser(@RequestParam int id){
+    return db.findUser(id);
+    }
+
+    //Search a user by user_id
+    //localhost:8090/search/user?id=101 -path param
+    @GetMapping("/search/user/{id}")
+    public User findAUserWithPathParam(@PathVariable int id){
+        return db.findUser(id);
+    }
+
+
 }
---
+++ src/main/java/com/example/MyApp/demo/Db.java
@@ -0,0 +1,32 @@
+package com.example.MyApp.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Db {
+
+    private ArrayList<User> userTable= new ArrayList<>();
+
+    //Inserting dummy data into the list
+    public Db()
+    {
+        userTable.add(new User(101,"Rami",202));
+        userTable.add(new User(102,"Ashu",203));
+    }
+
+    public boolean addUser(User user){
+        userTable.add(user);
+        return true;
+    }
+
+    public List<User> getAllUsers(){
+        return userTable;
+    }
+    public User findUser(int id){
+        for(User user:userTable){
+            if(user.getId()==id) return user;
+        }
+        System.out.println("Could not find the user");
+        return null;
+    }
+}
